5c5df083566fb4fb96cee642195d48f7dcf2348a
perneszmatyi <59794751+perneszmatyi@users.noreply.github.com>
2023-12-19 13:14:14 +0000

    added grid function

--- main.py
+++ main.py
@@ -89,7 +89,16 @@ def h(p1, p2):
     return abs(x1-x2) + abs(y1-y2)
 
 
-    
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Spot(i, j, gap, rows)
+            grid[i].append(spot)
+
+    return grid
     
 
 
