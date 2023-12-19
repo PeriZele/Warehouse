ae7e9faba96f0b83580c3bcbb3f0f072703e39cc
perneszmatyi <59794751+perneszmatyi@users.noreply.github.com>
2023-12-19 13:07:56 +0000

    added methods for spot class

--- main.py
+++ main.py
@@ -25,10 +25,64 @@ class Spot:
         self.x = row * width
         self.y = col * width
         self.color =  WHITE
-        self.neighbour = []
+        self.neighbor = []
         self.width = width
         self.total_rows = total_rows
 
 
     def get_pos(self):
         return self.row, self.col
+    
+
+    def is_closed(self):
+        return self.color == RED
+    
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_barrier(self):
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQOISE
+    
+    def reset(self):
+        return self.color == WHITE
+    
+     
+    def make_closed(self):
+        self.color = RED
+    
+    def make_open(self):
+        self.color = GREEN
+    
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_start(self):
+        self.color = ORANGE
+    
+    def make_end(self):
+        self.color = TURQOISE
+    
+    def make_path(self):
+        self.color = PURPLE
+
+    
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+    
+    
+
+
+
